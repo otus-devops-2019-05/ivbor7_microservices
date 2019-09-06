2952653c2e1769d67f8ac2ab9c78eba94c32a737
Ivan Boriskin <ivbor7@gmail.com>
2019-09-06 16:30:31 +0300

    Created Makefile to automate image building and pushing process

--- src/comment/docker_build.sh
+++ src/comment/docker_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+DateStr=$(date +"%Y-%m-%d %H:%M:%S")
+USER_NAME=ivbdockerhub
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
+echo "$DateStr docker_build script is running in comment for $USER_NAME Registry" >> ~/Otus/ivbor7_microservices/make_info.txt
 
 docker build -t $USER_NAME/comment .
+
+echo "$DateStr docker_build script completed building $USER_NAME/comment" >> ~/Otus/ivbor7_microservices/make_info.txt
--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+DateStr=$(date +"%Y-%m-%d %H:%M:%S")
+USER_NAME=ivbdockerhub
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
+echo "$DateStr docker_build script is running in post-py for $USER_NAME Registry" >> ~/Otus/ivbor7_microservices/make_info.txt
 
 docker build -t $USER_NAME/post .
+
+echo "$DateStr docker_build script completed building $USER_NAME/post" >> ~/Otus/ivbor7_microservices/make_info.txt
