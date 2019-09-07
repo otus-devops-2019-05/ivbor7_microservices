3024a33a1f124d909750a4e4ff74ee90410da441
Ivan Boriskin <ivbor7@gmail.com>
2019-09-07 09:27:33 +0300

    Var USER_NAME in docker_build.sh commented

--- src/post-py/docker_build.sh
+++ src/post-py/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DateStr=$(date +"%Y-%m-%d %H:%M:%S")
-USER_NAME=ivbdockerhub
+#USER_NAME=ivbdockerhub
 
 echo `git show --format="%h" HEAD | head -1` > build_info.txt
 echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
